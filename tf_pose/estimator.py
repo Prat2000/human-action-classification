dae0043618d951024538c9f541447b97d794e546
Pratikhya Ranjit <60696998+Prat2000@users.noreply.github.com>
2020-06-13 12:50:07 +0530

    Update estimator.py

--- tf_pose/estimator.py
+++ tf_pose/estimator.py
@@ -5,8 +5,10 @@ import slidingwindow as sw
 
 import cv2
 import numpy as np
-import tensorflow as tf
 import time
+import tensorflow as tf
+# import tensorflow.compat.v1 as tf   #added
+# tf.disable_v2_behaviour()   #added
 
 from tf_pose import common
 from tf_pose.common import CocoPart
